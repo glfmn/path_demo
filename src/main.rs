586a9c6dac7dccb3cd1487c7b6a2c324bf89ce55
Gwen Lofman <Gwen@Lofman.io>
2019-01-18 10:49:03 -0500

    Encapsulate map drawing and rng

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate tcod;
 
 mod draw;
 
-use game_lib::map::{generate, Tile};
+use game_lib::map::{generate, Map, Tile};
 use rand::thread_rng;
 use tcod::colors::{self, Color};
 use tcod::console::*;
@@ -27,6 +27,26 @@ const COLOR_GROUND: Color = Color { r: 50, g: 50, b: 150 };
 // Color of the cursor and other UI elements
 const COLOR_CURSOR: Color = Color { r: 200, g: 180, b: 50 };
 
+fn draw_map(root: &mut Root, map_layer: &mut Offscreen, map: &Map) {
+    map_layer.clear();
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            let color = if map[(x, y)].is_wall() { COLOR_WALL } else { COLOR_GROUND };
+            map_layer.set_char_background(x as i32, y as i32, color, BackgroundFlag::Set);
+        }
+    }
+
+    blit(
+        map_layer,
+        (0, 0),
+        (SCREEN_WIDTH as i32, SCREEN_HEIGHT as i32),
+        root,
+        (0, 0),
+        1f32,
+        1f32,
+    );
+}
+
 fn main() {
     let mut root = Root::initializer()
         .font("prestige12x12_gs_tc.png", FontLayout::Tcod)
@@ -39,9 +59,8 @@ fn main() {
 
     tcod::system::set_fps(30);
 
-    let mut rng = thread_rng();
-
-    let mut map = generate(&mut rng, MAP_WIDTH, MAP_HEIGHT);
+    let mut map_rng = thread_rng();
+    let mut map = generate(&mut map_rng, MAP_WIDTH, MAP_HEIGHT);
 
     let mut mouse = Default::default();
     let mut key = Default::default();
@@ -55,32 +74,16 @@ fn main() {
         use tcod::input::KeyCode::{Backspace, Escape};
         match key {
             Key { code: Escape, .. } => break,
-            Key { code: Backspace, .. } => map = generate(&mut rng, MAP_WIDTH, MAP_HEIGHT),
+            Key { code: Backspace, .. } => map = generate(&mut map_rng, MAP_WIDTH, MAP_HEIGHT),
             _ => (),
         };
 
-        map_layer.clear();
-        for y in 0..MAP_HEIGHT {
-            for x in 0..MAP_WIDTH {
-                let color = if map[(x, y)].is_wall() { COLOR_WALL } else { COLOR_GROUND };
-                map_layer.set_char_background(x as i32, y as i32, color, BackgroundFlag::Set);
-            }
-        }
+        draw_map(&mut root, &mut map_layer, &map);
 
         if let Some(&Tile::FLOOR) = map.get(mouse.cx as u32, mouse.cy as u32) {
-            map_layer.put_char(mouse.cx as i32, mouse.cy as i32, 'X', BackgroundFlag::None);
-            map_layer.set_char_foreground(mouse.cx as i32, mouse.cy as i32, COLOR_CURSOR);
+            root.put_char(mouse.cx as i32, mouse.cy as i32, 'X', BackgroundFlag::None);
+            root.set_char_foreground(mouse.cx as i32, mouse.cy as i32, COLOR_CURSOR);
         }
-
-        blit(
-            &mut map_layer,
-            (0, 0),
-            (SCREEN_WIDTH as i32, SCREEN_HEIGHT as i32),
-            &mut root,
-            (0, 0),
-            1f32,
-            1f32,
-        );
 
         root.flush();
     }
